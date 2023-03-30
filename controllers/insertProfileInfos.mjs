782d032cabf8457f904415d72d72c68f0914375e
audricdet <audric.det@hotmail.com>
2023-03-30 09:33:11 +0200

    feature insertProfileInfos

---
+++ controllers/insertProfileInfos.mjs
@@ -0,0 +1,38 @@
+import client from "../src/db/connect.mjs";
+
+const insertProfileInfos = async (request, response) => {
+    console.log(request.body)
+    const {
+        firstname,
+        lastname,
+        email,
+        phone,
+        user_id
+    } = request.body;
+
+await client.query(
+        `INSERT INTO profile (name,
+            lastname,
+            email,
+            phone,
+            adress, 
+            user_id)
+        VALUES ($1, $2, $3, $4, $5, $6)`,
+        [   firstname,
+            lastname,
+            email,
+            phone, 
+            user_id],
+        (error, result) => {
+            if (error) {
+                throw error;
+            }else{
+                console.log(result)
+                response.send({result: result})
+            }
+            
+        }
+    );
+};
+
+export default insertProfileInfos;
