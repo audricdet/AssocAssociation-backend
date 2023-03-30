ffc7a95c508cdc74778798bd458eef6a2953c013
audricdet <audric.det@hotmail.com>
2023-03-30 09:38:16 +0200

    feature insertProfileInfos

--- controllers/insertProfileInfos.mjs
+++ controllers/insertProfileInfos.mjs
@@ -14,8 +14,7 @@ await client.query(
         `INSERT INTO profile (name,
             lastname,
             email,
-            phone,
-            adress, 
+            phone, 
             user_id)
         VALUES ($1, $2, $3, $4, $5, $6)`,
         [   firstname,
