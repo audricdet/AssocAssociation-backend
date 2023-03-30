cf4c3709b70a7682aee288de26790c75f96eff7b
audricdet <audric.det@hotmail.com>
2023-03-30 10:08:28 +0200

    feature getAllAssociations

--- controllers/getAllAssociations.mjs
+++ controllers/getAllAssociations.mjs
@@ -3,11 +3,20 @@ import client from '../src/db/connect.mjs';
 const getAllAssociations = async (response) => {
     try {
         const result = await client.query('SELECT * FROM associations');
-        response.status(200).json(result.rows);
+        if (response && response.status) { // Vérifier si response est défini et contient une méthode status
+            response.status(200).json(result.rows);
+        } else {
+            console.error('L\'objet response est incorrectement défini');
+        }
     } catch (error) {
         console.error(error);
-        response.status(500).send('Erreur serveur');
+        if (response && response.status) { // Vérifier si response est défini et contient une méthode status
+            response.status(500).send('Erreur serveur');
+        } else {
+            console.error('L\'objet response est incorrectement défini');
+        }
     }
 };
+
 
 export default getAllAssociations;
