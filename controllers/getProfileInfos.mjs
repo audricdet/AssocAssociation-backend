89667a246c58e31fe1952d98efed12552309499e
audricdet <audric.det@hotmail.com>
2023-04-05 16:14:11 +0200

    getProfileInfos modified

--- controllers/getProfileInfos.mjs
+++ controllers/getProfileInfos.mjs
@@ -1,11 +1,11 @@
 import client from '../src/db/connect.mjs';
 
 const getProfileInfos = (request, response) => {
-    const id = request.params.id;
+    const user_id = request.params.user_id;
 
     client.query(
         'SELECT * FROM profile WHERE id = $1',
-        [id],
+        [user_id],
         (error, result) => {
             if (error) {
                 response.status(500).json({ error });
