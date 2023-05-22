6aeec0dc1b1a3fcddbc16a46367de623cfada1e5
audricdet <audric.det@hotmail.com>
2023-05-22 20:55:21 +0200

    Add edit profile

--- controllers/editProfileInfos.mjs
+++ controllers/editProfileInfos.mjs
@@ -1,23 +1,31 @@
-import client from '../src/db/connect.mjs';
+import client from "../src/db/connect.mjs";
 
-const editProfileInfos = async (req, res) => {
+const editProfileInfos = async (request, response) => {
+    console.log(request.body);
+    const {
+        user_id,
+        firstname,
+        lastname,
+        phone
+    } = request.body;
+
     try {
-    const { profileId, firstname, lastname, phone } = req.body;
+        const query = `
+            UPDATE "profile"
+            SET firstname = $1, lastname = $2, phone = $3
+            WHERE user_id = $4
+        `;
+        const values = [firstname, lastname, phone, user_id];
 
-    const query = `
-        UPDATE "profile"
-        SET "firstname" = $1, "lastname" = $2, "phone" = $3
-        WHERE "id" = $4
-    `;
-    const values = [firstname, lastname, phone, profileId];
+        const result = await client.query(query, values);
 
-    await client.query(query, values);
-
-    res.status(200).json({ message: 'Informations de profil mises à jour avec succès' });
-        } catch (error) {
-            console.error('Erreur lors de la mise à jour des informations de profil :', error);
-            res.status(500).json({ message: 'Une erreur est survenue lors de la mise à jour des informations de profil' });
+        console.log(result);
+        response.send({ result: result });
+    } catch (error) {
+        console.error("Erreur lors de la mise à jour des informations de profil :", error);
+        response.status(500).json({ message: "Une erreur est survenue lors de la mise à jour des informations de profil" });
     }
 };
 
 export default editProfileInfos;
+
